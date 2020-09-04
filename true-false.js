6c328ecc0a057033b4caf6d411889c757e07eaac
masum-webdev <masum.webdev@gmail.com>
2020-09-04 13:38:46 +0600

    truthy falsy added

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//falsy
+//false
+//0
+//""- empty space
+//undefined
+//null
+//NaN
+
+//truthy
+//"0"
+//" "-white space
+//[]
+//{}
+let temp=" ";
+if(temp){
+  console.log("condition is true.");
+}else{
+  console.log("condition is false.");
+}
