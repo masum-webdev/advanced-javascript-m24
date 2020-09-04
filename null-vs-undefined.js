ca6b3f6c3dbea98e934a973d4db9396170b4dba0
masum-webdev <masum.webdev@gmail.com>
2020-09-04 16:20:00 +0600

    null vs undefined example

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(n1,n2){
+//   console.log(n1+n2);
+//   return
+// }
+
+// function add(n1,n2){
+//   console.log(n2);
+// }
+
+// const premik={name:"alam",phone:018};
+// console.log(premik.gf);
+
+// let fun=undefined;
+
+// let ages=[10,20];
+// console.log(ages[3]);
+
+
+let temp=null;//null set korte hoi
+console.log(temp);
+
+
+
